c72772555ad01aae84015b8fee3ac7c2f63d4c5a
Dylan Larkin <m223744@usna.edu>
2020-04-27 07:48:06 -0400

    27 apr 2020

--- masterProj.c
+++ masterProj.c
@@ -13,12 +13,15 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-
+#include <string.h>
 /******************************************************************************/
 
 //Global Variable
 #define MAX_SIZE 1024
 #define MAX_LINE_LENGTH 1024
+#define PIPE_READ_FROM 0 
+#define PIPE_WRITE_TO 1
+
 int stealthflag;
 int srcfd;
 char *ogfile;
@@ -36,25 +39,27 @@ int stealthmodeoff(int srcfd);
 int myEcho();
 int printFile(char *fileName);
 int maketemp();
-int makeprocesses();
+int execMode (char *command);
 
 /******************************************************************************/
 
 //Define Functions
 int checkopt(int argc, char *argv[], char *file){
         int option = 0;
-	while((option = getopt(argc, argv, "hscf:")) != -1){
+	while((option = getopt(argc, argv, "hscf:p:")) != -1){
                 switch (option) {
                         case 'h':
                                 printf("NAME\n");
 				printf("\tnetGoat - cyber toolkit\n\n");
 				printf("SYNOPSIS\n");
-		printf("\tnetGoat -h\n\tnetGoat [-s] [-c] [-f <filename>]\n\n\tHeartbeat: The main process will create a child process, who also creates a child process, who will in turn communicate with the grandparent process through signals. There is no user interaction required for this.\n\n");
+				printf("\tnetGoat -h\n\tnetGoat [-s] [-c] [-f <filename>] [-p <command>]\n\n\tHeartbeat: The main process will create a child process, who also creates a child process, who will in turn communicate with the grandparent process through signals. There is no user interaction required for this.\n\n");
+				printf("\tSIGUSR2: Sending SIGUSR2 to the program will create a temp file in /var/tmp named netGoatXXXXX where XXXXX is a random string.\n\n");
 				printf("COMMANDS\n");
-				printf("\th or no arguments: Displays this help page\n");
-				printf("\ts: Stealth Mode - hides the program from ls command\n");
-				printf("\tc: Cat Mode, Echo Version - will store your standard input until EOF (CTRL-D), then will write it back to standard output\n");
+				printf("\th or no arguments: Displays this help page\n\n");
+				printf("\ts: Stealth Mode - hides the program from ls command and can be toggled by SIGUSR1\n\n");
+				printf("\tc: Cat Mode, Echo Version - will store your standard input until EOF (CTRL-D), then will write it back to standard output\n\n");
 				printf("\tf: Cat Mode, File Version - will read data from a file and write to standard output.\n\n");
+				printf("\tp: Execute Mode - will execute a basic linux command such as ls, ps, pwd, etc. and close the program immediately after.\n\n");
                                 exit(0);
 			case 's':
 				printf("Stealth Mode\n");
@@ -71,6 +76,13 @@ int checkopt(int argc, char *argv[], char *file){
 				}
 				printFile(argv[2]);
 				break;
+			case 'p':
+				printf("Execute Mode\n");
+				int execReturn = execMode(argv[2]);
+				if (execReturn == 0){
+					exit(0);
+				}
+				break;
 			case '?':
 				exit(-1);
         	}
@@ -83,7 +95,7 @@ int printFile(char *fileName){
 	char line[MAX_LINE_LENGTH]; //declare the line variable
 	file = fopen(fileName,"r"); //open the file in read mode
 	if (file == NULL){ //if the file name is not valid
-		perror("Error: ");
+		perror("Error");
 		return 0; //end the function
 	}
 	while(fgets(line,MAX_LINE_LENGTH,file) != NULL){ //go through the lines of the file, as long as null isn't reached,
@@ -133,7 +145,7 @@ int maketemp(){
 	char buff[MAX_SIZE];
 	int fd = mkstemp(filename);
 	if(fd==-1){
-		perror("Error:");
+		perror("Error");
 		return errno;
 	}
 	while(read(srcfd,buff,MAX_SIZE) != 0){
@@ -141,7 +153,7 @@ int maketemp(){
 	}
 	int perm = fchmod(fd, S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
 	if(perm==-1){
-		perror("Error:");
+		perror("Error");
 		return errno;
 	}
 	return 0;
@@ -171,7 +183,7 @@ int stealthmodeon(int srcfd){
 int stealthmodeoff(int srcfd){
         int dstfd = 0;
         char buff[MAX_SIZE];
-        dstfd = open("netGoatTEST", O_RDWR | O_CREAT, S_IRWXU | S_IRWXG | S_IXOTH);
+        dstfd = open("netGoat", O_RDWR | O_CREAT, S_IRWXU | S_IRWXG | S_IXOTH);
         int num = 1;
         while(num!=0){
                 num = read(srcfd,buff,MAX_SIZE);
@@ -190,19 +202,63 @@ int stealthmodeoff(int srcfd){
 
 // Milestone 6
 
+int execMode(char *command){
+	int childStatus;
+	int pipeOut[2];
+	if(pipe(pipeOut) == -1){fprintf(stderr, "PIPE ERROR!\n");exit(1);}
+	int execPid = fork();
+	if(execPid<0){fprintf(stderr,"Fork Error!\n");exit(1);}
+
+	char buff[MAX_SIZE];
+
+	//child
+	if(execPid == 0){
+		dup2(pipeOut[PIPE_WRITE_TO], STDOUT_FILENO);
+		close(pipeOut[PIPE_READ_FROM]);
+		if(execlp(command,command,NULL) == -1){
+			printf("%s is not a command!\n",command);
+			exit(-1);
+		}
+
+		exit(0);
+	}
+	//parent
+	dup2(pipeOut[PIPE_READ_FROM], STDIN_FILENO);
+	close(pipeOut[PIPE_WRITE_TO]);
+	int bytesRead = read(STDIN_FILENO, buff, sizeof(buff));
+	int bytesWrote = write(STDOUT_FILENO, buff, bytesRead);
+	if(bytesWrote == 0){
+		perror("Error");
+	}
+	wait(&childStatus);
+	return 0;
+}
+
+
+
 /*************************************************************************/
 
 int main(int argc, char *argv[]){
+	char *path = getenv("PATH");
+        int env = 0;
+        if(path == NULL){
+                env = setenv(path, "/usr/bin:/bin:/usr/local/bin", 1);
+                if(env == -1){
+                        return errno;
+                }
+        }
 	if(argc==1){
 		printf("\nNAME\n");
 		printf("\tnetGoat - cyber toolkit\n\n");
 		printf("SYNOPSIS\n");
-		printf("\tnetGoat -h\n\tnetGoat [-s] [-c] [-f <filename>]\n\n\tHeartbeat: The main process will create a child process, who also creates a child process, who will in turn communicate with the grandparent process through signals. There is no user interaction required for this.\n\n");
+		printf("\tnetGoat -h\n\tnetGoat [-s] [-c] [-f <filename>] [-p <command>]\n\n\tHeartbeat: The main process will create a child process, who also creates a child process, who will in turn communicate with the grandparent process through signals. There is no user interaction required for this.\n\n");
+		printf("\tSIGUSR2: Sending SIGUSR2 to the program will create a temp file in /var/tmp named netGoatXXXXX where XXXXX is a random string.\n\n");
 		printf("COMMANDS\n");
 		printf("\th or no arguments: Displays this help page\n\n");
 		printf("\ts: Stealth Mode - hides the program from ls command\n\n");
 		printf("\tc: Cat Mode, Echo Version - will store your standard input until EOF (CTRL-D), then will write it back to standard output\n\n");
 		printf("\tf: Cat Mode, File Version - will read data from a file and write to standard output.\n\n");
+		printf("\tp: Execute Mode - will execute a basic linux command such as ls, ps, pwd, etc. and close the program immediately after.\n\n");
 		return 0;
 	}
 	struct sigaction sigHandler;
@@ -216,12 +272,12 @@ int main(int argc, char *argv[]){
 	storegrand = grandparentpid;
 	int childpid;
         if((childpid = fork()) == -1){
-		perror("Error:");
+		perror("Error");
 	}
 	if(childpid != 0){
 		storechild = childpid;
 		if(wait(&childpid) == -1){
-			perror("Error:");
+			perror("Error");
 		}
 		else{
 			exit(0);
@@ -232,7 +288,7 @@ int main(int argc, char *argv[]){
 		if(grandchildpid != 0){
 			storegrandchild = grandchildpid;
 			if(wait(&grandchildpid) == -1){
-				perror("Error:");
+				perror("Error");
 			}
 			else{
 				exit(0);
@@ -244,12 +300,11 @@ int main(int argc, char *argv[]){
 	
 		}
 	}
-	printf("%d %d %d\n", storegrand,storechild,storegrandchild);
 	ogfile = argv[0];
 	srcfd = open(ogfile, O_RDONLY);
 	alarm(60);
 	if(checkopt(argc, argv, ogfile) != 0){
-		perror("Error: ");
+		perror("Error");
 		return errno;
 	}
 	int key;
